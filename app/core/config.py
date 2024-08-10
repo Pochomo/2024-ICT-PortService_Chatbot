6366423fb997e8b86d4d1e7de8ba25beabd45fd5
박준규 <junju404@naver.com>
2024-08-11 02:39:21 +0900

    Feat: 환경변수 등록

--- app/core/config.py
+++ app/core/config.py
@@ -5,7 +5,13 @@ load_dotenv()
 
 class Settings:
     OPENAI_API_KEY: str = os.getenv("OPENAI_API_KEY")
-    MILVUS_HOST: str = os.getenv("MILVUS_HOST", "localhost")
-    MILVUS_PORT: str = os.getenv("MILVUS_PORT", "19530")
+    MILVUS_HOST: str = os.getenv("MILVUS_HOST")
+    MILVUS_PORT: str = os.getenv("MILVUS_PORT")
+    
+    # RDS 설정 추가
+    DB_HOST: str = os.getenv("DB_HOST")
+    DB_USER: str = os.getenv("DB_USER")
+    DB_PASSWORD: str = os.getenv("DB_PASSWORD")
+    DB_NAME: str = os.getenv("DB_NAME")
 
 settings = Settings()
