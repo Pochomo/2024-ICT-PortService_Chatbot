d14eeb41eafcd1763404b9baed30d837929bf240
박준규 <junju404@naver.com>
2024-08-13 03:52:29 +0900

    Fix: 환경변수, 객체로 변경

--- app/core/config.py
+++ app/core/config.py
@@ -1,17 +1,17 @@
-import os
-from dotenv import load_dotenv
+from pydantic_settings import BaseSettings
 
-load_dotenv()
+class Settings(BaseSettings):
+    OPENAI_API_KEY: str
+    MILVUS_HOST: str
+    MILVUS_PORT: str
+    DB_HOST: str
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_NAME: str
+    ELASTICSEARCH_HOST: str
+    ELASTICSEARCH_PORT: str
 
-class Settings:
-    OPENAI_API_KEY: str = os.getenv("OPENAI_API_KEY")
-    MILVUS_HOST: str = os.getenv("MILVUS_HOST")
-    MILVUS_PORT: str = os.getenv("MILVUS_PORT")
-    
-    # RDS 설정 추가
-    DB_HOST: str = os.getenv("DB_HOST")
-    DB_USER: str = os.getenv("DB_USER")
-    DB_PASSWORD: str = os.getenv("DB_PASSWORD")
-    DB_NAME: str = os.getenv("DB_NAME")
+    class Config:
+        env_file = ".env"
 
 settings = Settings()
