a433f52f70cda4942ac4b791817bd97ed85a4c42
박준규 <junju404@naver.com>
2024-08-13 03:57:52 +0900

    Fix: 랭체인 document loader로 변경

--- app/services/hwp_reader.py
+++
@@ -1,6 +0,0 @@
-import olefile
-
-def read_hwp(filepath: str):
-    with olefile.OleFileIO(filepath) as f:
-        encoded_text = f.openstream('BodyText/Section0').read()
-    return encoded_text.decode('utf-16')
