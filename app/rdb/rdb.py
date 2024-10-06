b6eac117ac5f5fcf81fe6141529976c7b60f2f45
Pochomo <xorkd119@gmail.com>
2024-10-06 19:00:06 +0900

    temp

--- app/rdb/rdb.py
+++ app/rdb/rdb.py
@@ -1,24 +1,24 @@
-# app/database.py
-import os
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from app.core.config import settings
+# # app/database.py
+# import os
+# from sqlalchemy import create_engine
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import sessionmaker
+# from app.core.config import settings
 
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-load_dotenv()
-DATABASE_URL = os.getenv('RDB_URL')
+# load_dotenv()
+# DATABASE_URL = os.getenv('RDB_URL')
 
 
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
+# engine = create_engine(DATABASE_URL)
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# Base = declarative_base()
 
-# Dependency for getting the database session
-def get_rdb():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+# # Dependency for getting the database session
+# def get_rdb():
+#     db = SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
